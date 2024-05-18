1ce2fde7d2eb85b7d031b14de068056a14b85b20
sandesh kankriya <kankriyask13@gmail.com>
2024-05-18 15:40:19 +0530

    first commit

---
+++ frontend/src/components/shared/CloudinaryUpload.js
@@ -0,0 +1,37 @@
+import { cloudinary_upload_preset } from "../../config";
+import {openUploadWidget} from "../../utils/CloudinaryService";
+
+
+const CloudinaryUpload = ({setUrl, setName}) => {
+    const uploadImageWidget = () => {
+        let myUploadWidget = openUploadWidget(
+            {
+                cloudName: "btechproject2023",
+                uploadPreset: cloudinary_upload_preset,
+                sources: ["local"],
+            },
+            function (error, result) {
+                if (!error && result.event === "success") {
+                    setUrl(result.info.secure_url);
+                    setName(result.info.original_filename);
+                } else {
+                    if (error) {
+                        console.log(error);
+                    }
+                }
+            }
+        );
+        myUploadWidget.open();
+    };
+
+    return (
+        <button
+            className="bg-white text-black  rounded-full p-4 font-semibold"
+            onClick={uploadImageWidget}
+        >
+            Select Track
+        </button>
+    );
+};
+
+export default CloudinaryUpload;
